ae380b2690ad29f8c5a20ae34de07d7d6e1b6e0e
kellie <kmjos@ncsu.edu>
2015-09-21 23:54:00 -0400

    Added verifications on user attributes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+  has_many :books
+
+  has_secure_password validations: false
+
+  validates :email, presence: true,
+            length: { within: 5..20 },
+            uniqueness: true
+
+  validates :password, presence: true,
+            length: { within: 5..20 }
+
+  validates :id, presence: true
+
+  validates :name, presence: true
+
+  validates :status, presence: true
 end
