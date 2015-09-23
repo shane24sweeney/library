6b2720d7b199896694289a2aaf4bc7edef18c49d
kellie <kmjos@ncsu.edu>
2015-09-23 01:43:59 -0400

    I added in the annotation to state that users have books and the books belong to a user. Also added links on the user profile page to return books and checkout books. The links currently redirect to user/index. This needs to be updated when the checkout/return functionality is implemented.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ActiveRecord::Base
+  belongs_to :user
+  validates :user_id, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :users
   resources :admins
   resources :books
-  resources :profiles, only: [:edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
