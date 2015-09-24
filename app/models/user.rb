90bd91160bc27c7644104ef55a5e3070f139b25f
Moharnab Saikia <moharnab123saikia@gmail.com>
2015-09-23 23:17:24 -0700

    added validations for User model added code for title generation of pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+   validates :name,  presence: true, length: { minimum: 5, maximum: 60 }
+   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
+             uniqueness: { case_sensitive: false }
+   validates :password, presence: true, length: {minimum: 6}
    has_many :books
 end
